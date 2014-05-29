9395f159d63eb2be6f36e1759ebd5266e12a4929
Sabrina Beck Angelini <ra157240@oliveira.lab.ic.unicamp.br>
2014-05-29 18:19:14 -0300

    Adicionou geração de chave

--- lab09/lab09.c
+++ lab09/lab09.c
@@ -27,6 +27,7 @@ void ler();
 void lerCadastro();
 void lerBusca();
 void escrever();
+void gerarChave(Aluno aluno, int chave[2][2]);
 
 char nomeArquivo[TAM_NOME_ARQUIVO];
 int n;
@@ -35,8 +36,22 @@ char tipoEntrada; /* FIXME verificar se deve mesmo ser global */
 
 
 int main() {
-	ler();
-	escrever();
+/*	ler();
+	escrever();*/
+	int i, j;
+	int chave[2][2];
+	Aluno aluno;
+	aluno.nascimento.dia = 22;
+	aluno.nascimento.mes = 12;
+	aluno.nascimento.ano = 1996;
+	gerarChave(aluno, chave);
+
+	for(i = 0; i < 2; i++){
+		for(j = 0; j < 2; j++)
+			printf("%d ", chave[i][j]);
+		printf("\n");
+	}
+
 	return 0;
 }
 
@@ -90,11 +105,15 @@ void ler() {
 		lerBusca();
 }
 
+void escreverData(Data data) {
+	printf("Data Nascimento: %hu/%hu/%hu\n", data.dia, data.mes, data.ano);
+}
+
 void escreverAluno(Aluno aluno) {
 		printf("Nome: %s\n", aluno.nome);
 		printf("RA: %d\n", aluno.ra);
 		printf("Genero: %c\n", aluno.genero);
-		printf("Data Nascimento: %hu/%hu/%hu\n", aluno.nascimento.dia, aluno.nascimento.mes, aluno.nascimento.ano);
+		escreverData(aluno.nascimento);
 		printf("Genero que procura: %c\n\n", aluno.generoQueProcura);
 }
 
@@ -123,4 +142,11 @@ void escrever() { /* FIXME tirar isso */
 		escreverCadastro();
 	else
 		escreverBusca();
+}
+
+void gerarChave(Aluno aluno, int chave[2][2]) {
+	chave[0][0] = (aluno.nascimento.ano / 1000) * ((aluno.nascimento.ano % 1000) / 100);
+	chave[0][1] = (aluno.nascimento.mes / 10) * (aluno.nascimento.mes % 10);
+	chave[1][0] = (aluno.nascimento.dia / 10) * (aluno.nascimento.dia % 10);
+	chave[1][1] = ((aluno.nascimento.ano % 100) / 10) * (aluno.nascimento.ano % 10);
 }
